dc3d0d3f7258358c99cae1ae15e315de4cd48e28
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-10-15 21:59:14 +1100

    added queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+//Query to get user info , id etc etc
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
