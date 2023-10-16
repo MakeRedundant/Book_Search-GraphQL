c7dbc3757b4d1ee4794fb2330ae202de589dd0d4
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-10-16 23:17:46 +1100

    fixed connectjion.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,6 @@
 const mongoose = require('mongoose');
 
 //Mongoose to use external db if provided
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Google-books.graphql');
 
 module.exports = mongoose.connection;
